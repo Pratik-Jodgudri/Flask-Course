2192100c1a6d56ea055b87e27dc5ce1fffc7dedc
Pratik Jodgudri <87020427+Pratik-Jodgudri@users.noreply.github.com>
2024-06-01 23:09:01 +0530

    Add files via upload

---
+++ Part4/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from forms import SignupForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this_is_a_secret_key'
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
+
+@app.route('/signup', methods = ["GET", "POST"])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        flash(f'Successfully Registered {form.username.data}!')
+        return redirect(url_for('home'))
+    return render_template('signup.html', title='Sign Up', form=form)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    email = form.email.data
+    psw = form.password.data
+    if form.validate_on_submit():
+        if email == 'a@b.com' and psw == '12345':
+            flash(f'Logged in successfully!')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Invalid Email or Password')
+        
+    return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
